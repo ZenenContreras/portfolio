c1a13699fba5d6c64597310cb4a4dd9e1d5c62fd
Zenen Contreras Royero <zenencontreras1@gmail.com>
2025-10-05 18:18:38 -0500

    feat: enrutado de paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './index.css'
 import NavBar from './components/NavBar'
-import Home from './components/Home'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
 import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <div className='min-h-screen w-full overflow-x-hidden bg-[#090a0c] flex flex-col items-center'>
       <NavBar/>
-      <Home/>
-      <Skills/>
-      <Projects/>
+      <HomePage />
       <Footer />
     </div>
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from '../components/Home.jsx'
+import Skills from '../components/Skills.jsx'
+import Projects from '../components/Projects.jsx'
+
+
+export default function HomePage() {
+    return (
+        <>
+            <Home />
+            <Skills/>
+            <Projects />
+        </>
+    )
+}
