94b672916ca28bcaa68b667be565e39038cc1a2f
Zenen Contreras Royero <zenencontreras1@gmail.com>
2025-10-06 20:41:55 -0500

    feat: cambios eliminar el texto para que se limpie el formulario

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,6 @@ const Contact = () => {
 
     const handleSubmit = (event) =>{
         event.preventDefault()
-        console.log(refForm.current);
         
         const serviceId = 'service_cdypu4n'
         const templateId = 'template_ixcuo8c'
@@ -16,34 +15,38 @@ const Contact = () => {
         emailjs.sendForm(serviceId, templateId, refForm.current, apiKey )
         .then(result => console.log(result.text))
         .catch(error => console.error(error))
-        
+
+        if (refForm.current) {
+            refForm.current.reset();
+    }
     }
 
+
     return (
-        <div className='py-26 text-white flex flex-col items-center gap-10'>
-            <div className='text-center flex flex-col gap-4'>
+        <div className='py-26 text-white flex flex-col items-center gap-10 '>
+            <div className='text-center flex flex-col gap-4 w-[90%]'>
                 <h1 className='text-4xl mt-12 font-bold'>Contact</h1>
                 <p className='text-[#95999d]'>Open to new opportunities and collaborations. Let's build something together.</p>
             </div>
 
             <form ref={refForm} action="" onSubmit={handleSubmit} className='flex flex-col gap-10 text-left w-[90%] lg:w-full '>
                 <div className='flex flex-col gap-2 w-full '>
-                    <label htmlFor="">Email </label>
+                    <label htmlFor="">Email Adress</label>
                     <input required name='email' type="text" placeholder='Your@emailhere.com' className='border-1 border-white rounded-md px-7 py-2 w-full ' />
                 </div>
                 <div className='flex flex-col gap-2 w-full'>
                     <label htmlFor="">subject </label>
-                    <input required name='subject' type="text" placeholder='Let me know how can i help you ' className='border-1 border-white rounded-md px-7 py-2 w-full' />
+                    <input required name='subject' type="text" placeholder='Brief description of your inquiry' className='border-1 border-white rounded-md px-7 py-2 w-full' />
                 </div>
                 <div className='flex flex-col gap-2 w-full'>
                     <label htmlFor="">Leave a comment </label>
-                    <textarea required name='message' maxLength={500} type="text" placeholder='Your@emailhere.com' className='border-1 border-white rounded-md px-7 py-2 w-full' rows={5} cols={30} />
+                    <textarea required name='message' maxLength={500} type="text" placeholder='Tell me about the project, timeline, and how I can help...' className='border-1 border-white rounded-md px-7 py-2 w-full' rows={5} cols={30} />
                 </div>
 
                 
 
                 <div className='w-full justify-center flex items-center'>
-                    <button className='flex flex-row items-center gap-2 w-50 lg:w-50 justify-center border-1 border-white px-4 py-2 rounded-lg hover:border-blue-500 hover:text-blue-500 hover:scale-104 group transition duration-150'>
+                    <button type="submit" className='flex flex-row items-center gap-2 w-50 lg:w-50 justify-center border-1 border-white px-4 py-2 rounded-lg hover:border-blue-500 hover:text-blue-500 hover:scale-104 group transition duration-150'>
                         Send Message
                         <svg className="w-5 h-5 fill-white group-hover:fill-blue-500 transition duration-150 group-hover:-rotate-45" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/>
